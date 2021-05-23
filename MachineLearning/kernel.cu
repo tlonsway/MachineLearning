2042054e2bbe7af3dd19b902e4d914ad41a71a25
Tristan Lonsway <tblonswa@syr.edu>
2021-05-22 21:06:01 -0400

    Added matrix multiplication code

--- MachineLearning/kernel.cu
+++ MachineLearning/kernel.cu
@@ -1,121 +1,105 @@
-﻿
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cublas_v2.h>
+#include <curand.h>
 
-#include <stdio.h>
+// Fill the array A(nr_rows_A, nr_cols_A) with random numbers on GPU
+void GPU_fill_rand(float* A, int nr_rows_A, int nr_cols_A) {
+	// Create a pseudo-random number generator
+	curandGenerator_t prng;
+	curandCreateGenerator(&prng, CURAND_RNG_PSEUDO_DEFAULT);
 
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
+	// Set the seed for the random number generator using the system clock
+	curandSetPseudoRandomGeneratorSeed(prng, (unsigned long long) clock());
 
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
+	// Fill the array with random numbers on the device
+	curandGenerateUniform(prng, A, nr_rows_A * nr_cols_A);
 }
 
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
+// Multiply the arrays A and B on GPU and save the result in C
+// C(m,n) = A(m,k) * B(k,n)
+void gpu_blas_mmul(const float* A, const float* B, float* C, const int m, const int k, const int n) {
+	int lda = m, ldb = k, ldc = m;
+	const float alf = 1;
+	const float bet = 0;
+	const float* alpha = &alf;
+	const float* beta = &bet;
 
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
+	// Create a handle for CUBLAS
+	cublasHandle_t handle;
+	cublasCreate(&handle);
 
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
+	// Do the actual multiplication
+	cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, m, n, k, alpha, A, lda, B, ldb, beta, C, ldc);
 
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
+	// Destroy the handle
+	cublasDestroy(handle);
 }
 
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
 
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
+//Print matrix A(nr_rows_A, nr_cols_A) storage in column-major format
+void print_matrix(const float* A, int nr_rows_A, int nr_cols_A) {
 
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	for (int i = 0; i < nr_rows_A; ++i) {
+		for (int j = 0; j < nr_cols_A; ++j) {
+			std::cout << A[j * nr_rows_A + i] << " ";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
+}
 
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+int main() {
+	// Allocate 3 arrays on CPU
+	int nr_rows_A, nr_cols_A, nr_rows_B, nr_cols_B, nr_rows_C, nr_cols_C;
 
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
+	// for simplicity we are going to use square arrays
+	nr_rows_A = nr_cols_A = nr_rows_B = nr_cols_B = nr_rows_C = nr_cols_C = 3;
 
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	float* h_A = (float*)malloc(nr_rows_A * nr_cols_A * sizeof(float));
+	float* h_B = (float*)malloc(nr_rows_B * nr_cols_B * sizeof(float));
+	float* h_C = (float*)malloc(nr_rows_C * nr_cols_C * sizeof(float));
 
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	// Allocate 3 arrays on GPU
+	float* d_A, * d_B, * d_C;
+	cudaMalloc(&d_A, nr_rows_A * nr_cols_A * sizeof(float));
+	cudaMalloc(&d_B, nr_rows_B * nr_cols_B * sizeof(float));
+	cudaMalloc(&d_C, nr_rows_C * nr_cols_C * sizeof(float));
 
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
+	// If you already have useful values in A and B you can copy them in GPU:
+	// cudaMemcpy(d_A,h_A,nr_rows_A * nr_cols_A * sizeof(float),cudaMemcpyHostToDevice);
+	// cudaMemcpy(d_B,h_B,nr_rows_B * nr_cols_B * sizeof(float),cudaMemcpyHostToDevice);
 
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
+	// Fill the arrays A and B on GPU with random numbers
+	GPU_fill_rand(d_A, nr_rows_A, nr_cols_A);
+	GPU_fill_rand(d_B, nr_rows_B, nr_cols_B);
 
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
+	// Optionally we can copy the data back on CPU and print the arrays
+	cudaMemcpy(h_A, d_A, nr_rows_A * nr_cols_A * sizeof(float), cudaMemcpyDeviceToHost);
+	cudaMemcpy(h_B, d_B, nr_rows_B * nr_cols_B * sizeof(float), cudaMemcpyDeviceToHost);
+	std::cout << "A =" << std::endl;
+	print_matrix(h_A, nr_rows_A, nr_cols_A);
+	std::cout << "B =" << std::endl;
+	print_matrix(h_B, nr_rows_B, nr_cols_B);
 
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
+	// Multiply A and B on GPU
+	gpu_blas_mmul(d_A, d_B, d_C, nr_rows_A, nr_cols_A, nr_cols_B);
+
+	// Copy (and print) the result on host memory
+	cudaMemcpy(h_C, d_C, nr_rows_C * nr_cols_C * sizeof(float), cudaMemcpyDeviceToHost);
+	std::cout << "C =" << std::endl;
+	print_matrix(h_C, nr_rows_C, nr_cols_C);
+
+	//Free GPU memory
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_C);
+
+	// Free CPU memory
+	free(h_A);
+	free(h_B);
+	free(h_C);
+
+	return 0;
 }
