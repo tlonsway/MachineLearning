2f000524ce53f7087650f621affaab0d9a3ecc4c
Tristan Lonsway <tblonswa@syr.edu>
2021-05-23 20:21:36 -0400

    Added graphics library & progress on feedforward network

--- MachineLearning/layer.cpp
+++ MachineLearning/layer.cpp
@@ -63,12 +63,71 @@ float* FullyConnected::feedForward(const float* x) {
 
 void FullyConnected::backProp(const float* x, const float* y) {
 	//hard-coded using quadratic cost function
-
-	
-	//forward pass
-
-
+	float* nodes;
+	float* activations;
+	float* layerError;
+	int lTot = 0;
+	for (int i = 1; i < layerNum; i++) {
+		lTot += layers[i];
+	}
+	cudaMalloc(&nodes, sizeof(float) * lTot);
+	cudaMalloc(&activations, sizeof(float) * lTot);
+	cudaMalloc(&layerError, sizeof(float) * lTot);
+	int nodesOffset = 0;
+	int activationsOffset = 0;
+	int layerErrorOffset = 0;
+	//forward pass - xG will hold the result of forward propagation after this
+	float* xG;
+	cudaMalloc(&xG, sizeof(float) * layers[0]);
+	cudaMemcpy(xG, x, sizeof(float) * layers[0], cudaMemcpyHostToDevice);
+	int wMOffset = 0;
+	int bMOffset = 0;
+	for (int i = 0; i < layerNum - 1; i++) {
+		float* xGnext;
+		cudaMalloc(&xGnext, sizeof(float) * layers[i + 1]);
+		float* lyrWMat;
+		float* lyrBMat;
+		cudaMalloc(&lyrWMat, sizeof(float) * layers[i] * layers[i + 1]);
+		cudaMalloc(&lyrBMat, sizeof(float) * layers[i + 1]);
+		cudaMemcpy(lyrWMat, (wMat + wMOffset), sizeof(float) * layers[i] * layers[i + 1], cudaMemcpyDeviceToDevice);
+		cudaMemcpy(lyrBMat, (bMat + bMOffset), sizeof(float) * layers[i + 1], cudaMemcpyDeviceToDevice);
+		wMOffset += (layers[i] * layers[i + 1]);
+		bMOffset += (layers[i + 1]);
+		blas.gemmStandardFromGPUMem(lyrWMat, xG, xGnext, layers[i + 1], layers[i], 1);
+		blas.axpyStandardFromGPUMem(lyrBMat, xGnext, layers[i + 1]);
+		//SET NODES[layer] TO THIS BEFORE ACTIVATION FUNCTION IS APPLIED
+		cudaMemcpy((nodes+nodesOffset), xGnext, sizeof(float) * layers[i + 1], cudaMemcpyDeviceToDevice);
+		nodesOffset += layers[i + 1];
+		definedGPUFunctions::sigmoidMatCWiseGPUMem(xGnext, xGnext, layers[i + 1]);
+		//SET ACTIVATIONS[layer] TO THIS AFTER ACTIVATION FUNCTION IS APPLIED
+		cudaMemcpy((activations + activationsOffset), xGnext, sizeof(float) * layers[i + 1], cudaMemcpyDeviceToDevice);
+		activationsOffset += layers[i + 1];
+		cudaFree(lyrWMat);
+		cudaFree(lyrBMat);
+		cudaFree(xG);
+		cudaMalloc(&xG, sizeof(float) * layers[i + 1]);
+		cudaMemcpy(xG, xGnext, sizeof(float) * layers[i + 1], cudaMemcpyDeviceToDevice);
+		cudaFree(xGnext);
+	}
 	//calculate output error
+	float* tempComp;
+	cudaMalloc(&tempComp, sizeof(float) * layers[layerNum - 1]);
+	float* yG; //stores the "ideal" output of the network on the GPU memory 
+	cudaMalloc(&yG, sizeof(float) * layers[layerNum - 1]);
+	cudaMemcpy(yG, y, sizeof(float) * layers[layerNum - 1], cudaMemcpyHostToDevice);
+	definedGPUFunctions::subMatCWiseGPUMem(xG, yG, xG);
+	cudaMemcpy(tempComp, (nodes+lTot-layers[layerNum-1]), sizeof(float) * layers[layerNum - 1], cudaMemcpyDeviceToDevice);
+	definedGPUFunctions::sigmoidPrimeMatCWiseGPUMem(tempComp, tempComp, layers[layerNum - 1]); //tempComp now stores sigmoidPrime output of last layer	
+	definedGPUFunctions::multCompCWiseGPUMem(xG, tempComp, xG); //xG now stores the vector for the error on the output layer of the network
+	cudaMemcpy((layerError+lTot-layers[layerNum-1]), xG, sizeof(float) * layers[layerNum - 1], cudaMemcpyDeviceToDevice);
+	cudaFree(tempComp);
+	cudaFree(yG);
+	//backward pass
+	for (int i = layerNum - 3; i >= 0; i--) {
+		
+
+
+	}
 
 
 	//perform gradient descent
